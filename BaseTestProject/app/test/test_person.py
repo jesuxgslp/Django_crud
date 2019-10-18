6299511724ed702284509cb40fe5cc6f36dc5195
JesuX <jesusrq.gslp@gmail.com>
2019-10-18 13:10:34 -0500

    upss compy and paste error

--- BaseTestProject/app/test/test_person.py
+++ BaseTestProject/app/test/test_person.py
@@ -7,10 +7,10 @@ pytestmark = pytest.mark.django_db
 class TestPersonaModel:
 
     def test_save():
-        product = Persona.objects.create(
+        person = Persona.objects.create(
             nombre="Jose de Jesus",
             apellido="Reyes Quevedo",
             fecha_nacimiento="08/02/1987"
         )
-        assert product.name == "Jose de Jesus"
-        assert product.price == "Reyes Quevedo"
+        assert person.name == "Jose de Jesus"
+        assert person.price == "Reyes Quevedo"
