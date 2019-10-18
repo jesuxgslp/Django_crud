15a9beb6d46b910ce01ef5248a506ba63fb63df7
JesuX <jesusrq.gslp@gmail.com>
2019-10-18 13:24:29 -0500

    Copy selenum test form the llink

--- BaseTestProject/app/tests.py
+++ BaseTestProject/app/tests.py
@@ -1,34 +1,41 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-"""
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-import django
-from django.test import TestCase
+class AccountTestCase(LiveServerTestCase):
 
-# TODO: Configure your database in settings.py and sync before running tests.
+    def setUp(self):
+        self.selenium = webdriver.Firefox()
+        super(AccountTestCase, self).setUp()
 
-class ViewTest(TestCase):
-    """Tests for the application views."""
+    def tearDown(self):
+        self.selenium.quit()
+        super(AccountTestCase, self).tearDown()
 
-    if django.VERSION[:2] >= (1, 7):
-        # Django 1.7 requires an explicit setup() when running tests in PTVS
-        @classmethod
-        def setUpClass(cls):
-            super(ViewTest, cls).setUpClass()
-            django.setup()
+    def test_register(self):
+        selenium = self.selenium
+        #Opening the link we want to test
+        selenium.get('http://127.0.0.1:8000/accounts/register/')
+        #find the form element
+        first_name = selenium.find_element_by_id('id_first_name')
+        last_name = selenium.find_element_by_id('id_last_name')
+        username = selenium.find_element_by_id('id_username')
+        email = selenium.find_element_by_id('id_email')
+        password1 = selenium.find_element_by_id('id_password1')
+        password2 = selenium.find_element_by_id('id_password2')
 
-    def test_home(self):
-        """Tests the home page."""
-        response = self.client.get('/')
-        self.assertContains(response, 'Home Page', 1, 200)
+        submit = selenium.find_element_by_name('register')
 
-    def test_contact(self):
-        """Tests the contact page."""
-        response = self.client.get('/contact')
-        self.assertContains(response, 'Contact', 3, 200)
+        #Fill the form with data
+        first_name.send_keys('Yusuf')
+        last_name.send_keys('Unary')
+        username.send_keys('unary')
+        email.send_keys('yusuf@qawba.com')
+        password1.send_keys('123456')
+        password2.send_keys('123456')
 
-    def test_about(self):
-        """Tests the about page."""
-        response = self.client.get('/about')
-        self.assertContains(response, 'About', 3, 200)
+        #submitting the form
+        submit.send_keys(Keys.RETURN)
+
+        #check the returned result
+        assert 'Check your email' in selenium.page_source
